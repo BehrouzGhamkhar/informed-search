9ed89dab43f04f1fac7acaa08cfcd4551d318f26
Behrooz Ghamkhar <behroozghamkhar@gmail.com>
2024-05-11 12:53:17 +0100

    implemented the methods of Puzzle class

--- src/Puzzle.py
+++ src/Puzzle.py
@@ -11,8 +11,7 @@ class Puzzle:
         """
         self.init_state = init_state
         self.goal_state = [i for i in range(1, 9)] + [0]
-        # YOUR CODE HERE
-        raise NotImplementedError()
+        self.explored_set = set()
 
     def goal_test(self, state: List[int]):
         """Test if goal state is reached
@@ -20,7 +19,7 @@ class Puzzle:
         :return: true if the passed configuration is equal to goal configuration
         """
         # YOUR CODE HERE
-        raise NotImplementedError()
+        return state == self.goal_state
 
     def is_explored(self, state: List[int]):
         """Check if a particular board configuration has already been explored
@@ -28,11 +27,11 @@ class Puzzle:
         :return: true if a particular configuration has already been explored
         """
         # YOUR CODE HERE
-        raise NotImplementedError()
+        return state in self.explored_set
 
     def reset(self):
         # YOUR CODE HERE
-        raise NotImplementedError()
+        self.explored_set = set()
 
 
 def move_left(position: int) -> int:
@@ -41,7 +40,7 @@ def move_left(position: int) -> int:
     :return: new position of the 0 tile after moving to the left
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
+    return position + 1 if (position % 3) < 2 else position
 
 
 def move_right(position: int) -> int:
@@ -50,7 +49,7 @@ def move_right(position: int) -> int:
     :return: new position of the 0 tile after moving to the right
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
+    return position - 1 if (position % 3) > 0 else position
 
 
 def move_up(position: int) -> int:
@@ -59,7 +58,7 @@ def move_up(position: int) -> int:
     :return: new position of the 0 tile after moving upwards
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
+    return position + 3 if (position // 3) < 2 else position
 
 
 def move_down(position: int):
@@ -68,7 +67,7 @@ def move_down(position: int):
     :return: new position of the 0 tile after moving downwards
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
+    return position - 3 if (position // 3) > 0 else position
 
 
 def get_possible_moves(state: List[int]) -> List[List[int]]:
@@ -77,8 +76,26 @@ def get_possible_moves(state: List[int]) -> List[List[int]]:
     :return: list containing the new configurations after applying all possible moves
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
+    current_empty_tile = state.index(0)
+
+    def apply_move(move):
+        new_empty_tile = move(current_empty_tile)
+        if new_empty_tile != current_empty_tile:
+            new_state = state.copy()
+            new_state[current_empty_tile], new_state[new_empty_tile] = new_state[new_empty_tile], new_state[
+                current_empty_tile]
+            return new_state
+
+    move_lists = [apply_move(move_left), apply_move(move_right), apply_move(move_up), apply_move(move_down)]
+    possible_moves = [move for move in move_lists if move is not None]
+
+    return possible_moves
 
 
 # YOUR CODE HERE
-raise NotImplementedError()
+state = [1, 2, 3
+        ,4, 0, 6
+        ,7, 5, 8]
+
+moves = get_possible_moves(state)
+print(moves)
