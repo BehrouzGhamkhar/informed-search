c0c14af50aec172519597bf919f9cd3ff60a9671
Behrooz Ghamkhar <behroozghamkhar@gmail.com>
2024-05-11 20:46:35 +0100

    Implemented functions to generate random solvable puzzles and solved them.

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -1,42 +1,41 @@
 from Search import *
 import time
+import random
 
 
-def run_a_star(puzzle_configs, heuristic):
+def run_evaluation(puzzle_configs, heuristic, algorithm):
+
     records = []
     for i in puzzle_configs.keys():
         t0 = time.time()
         p = Puzzle(puzzle_configs[i])
-        path, expanded_nodes = astar_search(p, heuristic)
+        path, expanded_nodes = algorithm(p, heuristic)
         t1 = time.time()
 
         records.append({
             'puzzle': i,
-            'algorithm': 'astar',
+            'algorithm': algorithm.__name__,
             'heuristic': heuristic.__name__,
             'expanded nodes': expanded_nodes,
             'execution time': t1 - t0,
-            'path cost': len(path)})
+            'path cost': len(path),
+            'complexity': inversion(puzzle_configs[i])})
     return records
 
 
-def run_greedy(puzzle_configs, heuristic):
-    # Run this test cell in order to get an INDICATION of whether your implementation is working as expected.
-    records = []
-    for i in puzzle_configs.keys():
-        t0 = time.time()
-        p = Puzzle(puzzle_configs[i])
-        path, expanded_nodes = greedy_search(p, heuristic)
-        t1 = time.time()
+def evaluate(puzzle_configs):
+    heuristics = [manhattan_distance, hamming_distance]
+    search_algorithms = [astar_search, greedy_search]
+    evaluation_results = []
 
-        records.append({
-            'puzzle': i,
-            'algorithm': 'greedy',
-            'heuristic': heuristic.__name__,
-            'expanded nodes': expanded_nodes,
-            'execution time': t1 - t0,
-            'path cost': len(path)})
-    return records
+    for heuristic in heuristics:
+        for algorithm in search_algorithms:
+            eval_records = run_evaluation(puzzle_configs, heuristic, algorithm)
+            for record in eval_records:
+                evaluation_results.append(record)
+
+    for record in evaluation_results:
+        print(record)
 
 
 def inversion(configuration: List[int]) -> int:
@@ -66,19 +65,33 @@ def is_solvable(configuration: List[int]) -> bool:
     return inversion_count % 2 == 0
 
 
-def evaluate():
-    puzzle_configs = {1: [0, 1, 2, 3, 4, 5, 8, 6, 7],
-                      2: [8, 7, 6, 5, 1, 4, 2, 0, 3],
-                      3: [1, 5, 7, 3, 6, 2, 0, 4, 8]}
+def generate_puzzle():
+    """
+    Generate a random solvable puzzle configuration.
 
-    evaluation_results = [*run_a_star(puzzle_configs, manhattan_distance),
-                          *run_a_star(puzzle_configs, hamming_distance),
-                          *run_greedy(puzzle_configs, manhattan_distance),
-                          *run_greedy(puzzle_configs, hamming_distance)]
+    Returns:
+    :puzzle: A list representing the puzzle configuration
+    """
+    puzzle = list(range(9))
+    random.shuffle(puzzle)
+    while not is_solvable(puzzle):
+        random.shuffle(puzzle)
+    return puzzle
 
-    for record in evaluation_results:
-        print(record)
+
+def generate_solvable_puzzles(total):
+    return [generate_puzzle() for i in range(total)]
 
 
 if __name__ == "__main__":
-    evaluate()
+    # puzzle_configs = {1: [0, 1, 2, 3, 4, 5, 8, 6, 7],
+    #                   2: [8, 7, 6, 5, 1, 4, 2, 0, 3],
+    #                   3: [1, 5, 7, 3, 6, 2, 0, 4, 8]}
+
+    puzzle_configs = generate_solvable_puzzles(5)
+    puzzle_dict = {}
+
+    for index, config in enumerate(puzzle_configs):
+        puzzle_dict[index] = config
+
+    evaluate(puzzle_dict)
